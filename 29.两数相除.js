77c72271f8a9eacda37e12af7ddad2b5c61ccd76
Zihao <zihao@adoi.cn>
2022-06-26 22:08:25 +0800

    add two questions

---
+++ 29.两数相除.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=29 lang=javascript
+ *
+ * [29] 两数相除
+ */
+
+// @lc code=start
+/**
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+var divide = function (dividend, divisor) {
+  let result = parseInt(dividend / divisor)
+  if (result >= Math.pow(2, 31) - 1 || result < -Math.pow(2, 31)) {
+    return (Math.pow(2, 31) - 1);
+  } else {
+    return result
+  }
+};
+// @lc code=end
+
